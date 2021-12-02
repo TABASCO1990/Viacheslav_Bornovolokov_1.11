0aebadddde34993affef66280a50cda8a89dbeba
Viacheslav Born <64132803+TABASCO1990@users.noreply.github.com>
2021-12-02 12:56:46 +0500

    AddProject

---
+++ Helix Jump/Assets/Scripts/Platform.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scipts
+{
+    public class Platform : MonoBehaviour
+    {
+
+        public GameObject objScores;
+        public GameObject objRecord;
+        private Text scores;
+        private Text record;
+
+        private void Start()
+        {
+            objScores = GameObject.Find("Score"); // не забудь это говно переписать
+            scores = objScores.GetComponent<Text>();
+            objRecord = GameObject.Find("ScoresBest");// и это
+            record = objRecord.GetComponent<Text>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            
+            if (other.TryGetComponent(out Player player))
+            {
+                player.CurrentPlatform = this;
+                Game.scores++;
+                scores.text = Game.scores.ToString();
+                record.text = "Record: "+ Game.Record().ToString();
+            }
+        }
+    }
+}
